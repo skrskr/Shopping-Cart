4e231b8a214721a5f28fe81a75bf0845e75b4584
skrskr <mohamed1.sakr1996@gmail.com>
2019-11-23 11:03:45 +0200

    create index page to show products

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Shoping Cart' });
+  res.render('shop/index', { title: 'Shoping Cart' });
 });
 
 module.exports = router;
