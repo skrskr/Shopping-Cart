7fb8ccf8b586a65b9e900c66a49e6d9d68b4cce9
skrskr <mohamed1.sakr1996@gmail.com>
2019-11-27 15:35:41 +0200

    creating shopping cart view

--- routes/index_route.js
+++ routes/index_route.js
@@ -33,4 +33,12 @@ router.get('/add-to-cart/:id', (req, res, next) => {
   
 });
 
+router.get('/shopping-cart', (req, res, next) => {
+  if(!req.session.cart)
+      return res.render('shop/shopping-cart', {products: null});
+  
+  const cart = new Cart(req.session.cart);
+  res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
+})
+
 module.exports = router;
